19d499288dde23ee7a130c84b20f33520b3b23b5
Mike Sowers <msowers72@yahoo.com>
2021-06-06 16:07:53 -0400

    made anohter update to my javascript

--- Assets/script.js
+++ Assets/script.js
@@ -11,29 +11,24 @@ function generatePassword() {
   var passwordLength = 15;
   var password = "";
          for (var i = 0; i < passwordLength; i++) {
-      var calculate = Math.floor(Math.random() * dataInput.length);
-      password += dataInput.substring(calculate, calculate + 1);
+      var calculate = Math.floor(Math.random() * Math.floor(dataInput.length));
+      password += dataInput[calculate];
     } 
     return password;       
 } 
 
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
-
-function writePassword() {
-   
-
+function writePassword() { 
  var password = generatePassword();
  var passwordText = document.querySelector("#password");
  passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-//  var password = "password";
 
+// password += dataInput.substring(calculate, calculate + 1);
